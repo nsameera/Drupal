fa32d99c9d000e99466905dc141ee790ee28794b
Sameera Nanduri <sameera@hqsml-1202777.local>
2018-11-05 11:47:54 -0500

    second commit

---
+++ newmod/html_php.tpl.php
@@ -0,0 +1,14 @@
+
+
+<head>
+    <title>Display Date and Time in Javascript</title>
+    <script type="text/javascript" src="/script2.js"> </script>
+
+    <link rel="stylesheet" type="text/css" href="style.css" >
+
+</head>
+<body>
+  <h2>Current time is</h2>
+        <span id="date_time"></span>
+        <script type="text/javascript">window.onload = date_time('date_time');</script>
+</body>
