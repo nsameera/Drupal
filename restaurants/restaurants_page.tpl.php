87e6839edf2b148a3d944c056936b43f67d2e6b3
Sameera Nanduri <sameera@hqsml-1202777.local>
2018-11-05 11:22:48 -0500

    First Commit

---
+++ restaurants/restaurants_page.tpl.php
@@ -0,0 +1,6 @@
+
+<body>
+  <p><a href="http://drupal1.dd:8083/Appetizer">Appetizers</a></p>
+    <p><a href="http://drupal1.dd:8083/Main%20Course ">Main course</a></p>
+      <p><a href="http://drupal1.dd:8083/Dessert ">Dessert</a></p>
+    </body>
